8a6108a3df6e4857dd8c4431a9d121067097dba3
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-03-29 22:41:54 +0530

    adding reverseList function for reveseing list oa arrays

---
+++ arrays/reverseList.py
@@ -0,0 +1,16 @@
+
+A = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+def reverse(A):
+    m = len(A)
+    n = len(A[0])
+    B = []
+    for i in xrange(len(A)):
+        B.append([0]*n)
+        for j in xrange(len(A[i])):
+            B[i][n-1-j] = A[i][j]
+    
+    return B        
+
+
+print reverse(A)
