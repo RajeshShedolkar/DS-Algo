c9ef22c5dd5f46ac7a02d55f933bf8f113934b01
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-04-22 03:50:21 +0530

    adding comments

--- binary_search/trangle.py
+++ binary_search/trangle.py
@@ -1,13 +1,23 @@
+#this solution have time complexity O(n*n*log(n))
+
 def no_of_trangle(A):
+    #sorting array
     A.sort()
+    #initialization of high and low
     low = 0
     high = len(A)-1
+    #index will be element which we don't need to check beyond that index our solution will be upto that index
+    #total trangles
     total, index=0, 0
+    #first for loop which will start at 0 and itrate upto n-3
     for i in range(0, len(A)-2):
+    #2nd for loop 2nd element it will start at i+1 and itrate upto n-2
         for j in range(i+1, len(A)-1):
+            #for each new itration initilization for low and high and also for index
             low = j+1
             high = len(A)-1
             index = j
+            #binary search to find rightmost index
             while low<=high and low>j:
                 mid = (low+high)/2
                 if A[i]+A[j]>A[mid]:
