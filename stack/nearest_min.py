502b70684667edc40486df23fb3d6542cfd4df98
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-07-02 02:28:42 +0530

    Nearest Minimum element

---
+++ stack/nearest_min.py
@@ -0,0 +1,32 @@
+# nearest minimum element
+
+# time complexity  O(n*n)
+def prevSmaller(A):
+    min_val= A[0]
+    ret = [-1]*len(A)
+    for i in range(len(A)):
+        j = i-1
+        while j>=0:
+            if A[j]<A[i]:
+                ret[i] = A[j]
+                break
+            j-=1
+    return ret
+
+# order(n)
+def small(A):
+    st = []
+    ans = [-1]*len(A)
+    for i in range(len(A)):
+        while len(st)>0 and st[len(st)-1] >= A[i]:
+            st.pop()
+        if len(st) == 0 :
+            ans[i] = -1;
+        else: 
+            ans[i] = st[len(st)-1]
+        st.append(A[i])
+    return ans 
+
+A = [34, 35, 27, 42, 5, 28, 39, 20, 28]
+print prevSmaller(A)
+print small(A)
