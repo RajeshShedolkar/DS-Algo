0231281bc3669a9714c580205483b695fb9fdcec
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2016-07-12 14:42:23 +0530

    adding input for check BST

--- Tree/checkBST.py
+++ Tree/checkBST.py
@@ -1,20 +1,24 @@
-class TreeNode:
+class Node:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
 
-def isValidBST(self, node):
-    return int(self.checkBST(node, float('-inf'), float('inf')))
+def isValidBST(node):
+    return int(checkBST(node, float('-inf'), float('inf')))
 
-def checkBST(self, node, lower, upper):
+def checkBST(node, lower, upper):
     if node is None:
         return True
 
     return (lower < node.val < upper and
-            self.checkBST(node.left, lower, node.val) and
-            self.checkBST(node.right, node.val, upper))
+            checkBST(node.left, lower, node.val) and
+            checkBST(node.right, node.val, upper))
 
 
+root = Node(2)
+root.left = Node(1)
+root.right = Node(3)
+print isValidBST(root)
 
