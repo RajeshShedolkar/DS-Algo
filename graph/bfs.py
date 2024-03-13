5face8b496e3eb0b8a580eac55b0e4d9893856e3
Rajesh Muralidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-03-13 10:02:26 +0530

    added bfs file

---
+++ graph/bfs.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+    
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+    
+
+# Create a graph
+graph = Graph()
+ 
+# Add edges to the graph
+graph.addEdge(0, 1)
+graph.addEdge(0, 2)
+graph.addEdge(1, 3)
+graph.addEdge(1, 4)
+graph.addEdge(2, 4)
