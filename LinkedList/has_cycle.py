ebe4f751ce5606d4c9072eeedee4206b0244efb5
Rajesh Shedolkar <7788754+RajeshShedolkar@users.noreply.github.com>
2024-10-23 23:08:32 +0530

    Create has_cycle.py

---
+++ LinkedList/has_cycle.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def has_cycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+# Example usage:
+head = ListNode(3)
+head.next = ListNode(2)
+head.next.next = ListNode(0)
+head.next.next.next = ListNode(-4)
+head.next.next.next.next = head.next
+
+print(has_cycle(head))  # Output: True
