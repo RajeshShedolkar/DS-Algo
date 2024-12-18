8934937bcd4093d1a9e31f99d3ef2d7e8f6896ee
Rajesh Shedolkar <7788754+RajeshShedolkar@users.noreply.github.com>
2024-12-18 23:19:18 +0530

    Update waveArray.py

--- arrays/waveArray.py
+++ arrays/waveArray.py
@@ -1,9 +1,9 @@
 def wave(A):
-    A.sort()
+    A = sorted(A)
     for i in range(0, len(A), 2):
         if i+1< len(A):
             A[i],A[i+1]=A[i+1],A[i]
     return A
 
-A = [3,1,4,2,5]
-print wave(A)               
+A = [3, 1, 4, 2, 5]
+print(wave(A))         
